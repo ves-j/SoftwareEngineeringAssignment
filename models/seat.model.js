d871e72fc60cec3565825979d72c7344c32182fd
Oluwaseun Odueso <seunoduez@gmail.com>
2025-11-30 22:12:37 +0000

    chore: seat model

---
+++ models/seat.model.js
@@ -0,0 +1,33 @@
+// models/seat.model.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const seatSchema = new Schema({
+  seatNumber: {
+    type: String,
+    required: true
+  },
+  row: {
+    type: String,
+    required: true
+  },
+  section: {
+    type: String,
+    enum: ['stalls', 'circle', 'upperCircle'],
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  isAvailable: {
+    type: Boolean,
+    default: true
+  },
+  x: Number,
+  y: Number
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Seat', seatSchema);
