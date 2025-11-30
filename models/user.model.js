0b78fc6ddc49b1a72824380273cafdbc5c1b73b4
Oluwaseun Odueso <seunoduez@gmail.com>
2025-11-30 22:11:02 +0000

    chore: user model

---
+++ models/user.model.js
@@ -0,0 +1,48 @@
+// models/user.model.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true
+  },
+  isLoyaltyMember: {
+    type: Boolean,
+    default: false
+  },
+  loyaltySince: {
+    type: Date
+  }
+}, {
+  timestamps: true
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+module.exports = mongoose.model('User', userSchema);
