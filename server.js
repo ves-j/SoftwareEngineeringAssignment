8abd3e04dd2308ab934290a7d2cf59889b87ce34
Oluwaseun Odueso <seunoduez@gmail.com>
2026-01-01 14:37:41 +0000

    fix: cors fix

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
